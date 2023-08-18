f1f45cedee39f905d71a1bc11c82b76f10036a81
Maeda614 <leo.codemaestro@gmail.com>
2023-08-18 18:55:00 +0300

    chore: project structure init

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 ## project structure:
 
 ```
+
 ```
 
 ## installation
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"os"
+
+	"main.go/cli"
+)
+
+func main() {
+	defer os.Exit(0)
+
+	cmd := cli.CommandLine{}
+	cmd.Run()
+}
